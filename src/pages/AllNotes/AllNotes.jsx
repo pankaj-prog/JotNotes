c8cbcc49253cdd507c37502de8bf130eff7a7bf0
pankaj-prog <wadhwanip38@gmail.com>
2022-05-07 15:03:41 +0530

    feat: dummy pages and routing

---
+++ src/pages/AllNotes/AllNotes.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AllNotes = () => {
+  return <div>All Notes: userRoute</div>;
+};
+
+export default AllNotes;
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+export { default as Home } from "./Home/Home";
+export { default as AllNotes } from "./AllNotes/AllNotes";
+export { default as Trash } from "./Trash/Trash";
+export { default as Archive } from "./Archive/Archive";
+export { default as Login } from "./Login/Login";
+export { default as Signup } from "./Signup/Signup";

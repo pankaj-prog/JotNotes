e56f9082759411b91470dc46705494c7faa608df
pankaj-prog <wadhwanip38@gmail.com>
2022-05-08 18:05:20 +0530

    feat: custom axios hook

---
+++ src/utils/useAxios.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export const useAxios = () => {
+  const [response, setResponse] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const makeRequest = async (params) => {
+    try {
+      setLoading(true);
+      const res = await axios.request(params);
+      setResponse(res.data);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { response, error, loading, makeRequest };
+};
